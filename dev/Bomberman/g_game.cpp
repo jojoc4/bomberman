147d6e9ea135559ce14921e84f2914d67429f2c1
Téo Schaffner <teo.schaffner@he-arc.ch>
2018-11-12 13:19:34 +0100

    maquette fenêtre jeu et création classe de la fenêtre jeu

---
+++ dev/Bomberman/g_game.cpp
@@ -0,0 +1,6 @@
+#include "g_game.h"
+
+G_Game::G_Game(QWidget *parent) : QMainWindow(parent)
+{
+
+}
---
+++ dev/Bomberman/g_game.h
@@ -0,0 +1,17 @@
+#ifndef G_GAME_H
+#define G_GAME_H
+
+#include <QMainWindow>
+
+class G_Game : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit G_Game(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // G_GAME_H
