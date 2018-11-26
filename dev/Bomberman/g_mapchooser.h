c3cce56abbda043bed75f793e8b3c80a0dd471f2
Julien Chappuis <julien.chappuis@he-arc.ch>
2018-11-26 15:07:30 +0100

    Ajout d'une partie QGraphicView dans le widget de choix de carte

--- dev/Bomberman/g_mapchooser.h
+++ dev/Bomberman/g_mapchooser.h
@@ -1,7 +1,6 @@
 #ifndef G_MAPCHOOSER_H
 #define G_MAPCHOOSER_H
 
-#include <QDialog>
 #include <QtWidgets>
 
 class G_MapChooser : public QWidget
@@ -9,6 +8,9 @@ class G_MapChooser : public QWidget
     Q_OBJECT
 public:
     explicit G_MapChooser(QWidget *parent = nullptr);
+
+protected :
+
 
 signals:
 
@@ -21,6 +23,9 @@ private :
     QListWidget* listMaps;
     QDir* directory;
     QString filePath;
+
+    QGraphicsView* previewMap;
+
 };
 
 #endif // G_MAPCHOOSER_H
