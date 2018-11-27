73509cdde6043dcf6439c2d8632d361ce37e89b4
Jonatan Baumgartner <Jonatan.Baumgartner@he-arc.ch>
2018-11-27 09:03:39 +0100

    correction problème git

---
+++ dev/Bomberman/main.cpp
@@ -0,0 +1,22 @@
+#include "g_homescreen.h"
+#include "g_mapchooser.h"
+#include "g_game.h"
+#include "game.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    G_HomeScreen w;
+
+    G_MapChooser b(nullptr);
+
+    Game *game = new Game();
+    G_Game g(game, nullptr);
+
+    b.show();
+    //w.show();
+    //g.show();
+
+    return a.exec();
+}
